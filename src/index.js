acc7d1881287df7476bd087c08c6451d738de34e
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-26 11:46:06 -0700

    Refactor square to function component

--- src/index.js
+++ src/index.js
@@ -2,25 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
-  render() {
-    return (
-      <button  
-	className="square"
-	onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button  
+      className="square"
+      onClick={() => this.props.onClick()}
+    >
+      {this.props.value}
+    </button>
+  );
 }
+
 
 class Board extends React.Component {
   constructor(props) {
